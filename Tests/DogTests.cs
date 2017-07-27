e2382c6e6f21fef7605cbddfb8c5d2dcf9266df7
John <john@epicodus.com>
2017-07-26 17:09:35 -0700

    switch test framework to MS TEST

--- Tests/DogTests.cs
+++ Tests/DogTests.cs
@@ -1,37 +1,37 @@
-using Xunit;
-using System;
-
-namespace DogList.Objects
-{
-  public class DogListTests
-  {
-
-    [Fact]
-    public void Dog_HasName_True()
-    {
-      Dog newDog = new Dog("Kesa");
-      Assert.Equal("Kesa", newDog.GetName());
-    }
-
-    [Fact]
-    public void Dog_HasName_False()
-    {
-      Dog newDog = new Dog("Doug");
-      Assert.NotEqual("Kesa", newDog.GetName());
-    }
-
-    [Fact]
-    public void Dog_SavesToDatabase_True()
-    {
-      Dog newDog = new Dog("Doug");
-      newDog.Save();
-
-      Console.WriteLine(newDog.GetId());
-      Dog foundDog = Dog.Find(newDog.GetId());
-
-      Assert.Equal("Doug", foundDog.GetName());
-    }
-
-
-  }
-}
+// using Xunit;
+// using System;
+//
+// namespace DogList.Objects
+// {
+//   public class DogListTests
+//   {
+//
+//     [Fact]
+//     public void Dog_HasName_True()
+//     {
+//       Dog newDog = new Dog("Kesa");
+//       Assert.Equal("Kesa", newDog.GetName());
+//     }
+//
+//     [Fact]
+//     public void Dog_HasName_False()
+//     {
+//       Dog newDog = new Dog("Doug");
+//       Assert.NotEqual("Kesa", newDog.GetName());
+//     }
+//
+//     [Fact]
+//     public void Dog_SavesToDatabaseGetsAssignedId_True()
+//     {
+//       Dog newDog = new Dog("Doug");
+//       newDog.Save();
+//
+//       Console.WriteLine(newDog.GetId());
+//       Dog foundDog = Dog.Find(newDog.GetId());
+//
+//       Assert.Equal("Doug", foundDog.GetName());
+//     }
+//
+//
+//   }
+// }
---
+++ Tests/DogTestsMS.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DogList.Objects
+{
+  [TestClass]
+  public class DogListTests
+  {
+
+    [TestMethod]
+    public void Dog_HasName_True()
+    {
+      Dog newDog = new Dog("Kesa");
+      Assert.AreEqual("Kesa", newDog.GetName());
+    }
+
+    [TestMethod]
+    public void Dog_HasName_False()
+    {
+      Dog newDog = new Dog("Doug");
+      Assert.AreEqual("Kesa", newDog.GetName());
+    }
+
+    [TestMethod]
+    public void Dog_SavesToDatabaseGetsAssignedId_True()
+    {
+      Dog newDog = new Dog("Doug");
+      newDog.Save();
+
+      Console.WriteLine(newDog.GetId());
+      Dog foundDog = Dog.Find(newDog.GetId());
+
+      Assert.AreEqual("Doug", foundDog.GetName());
+    }
+
+
+  }
+}
